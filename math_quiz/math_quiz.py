fa77bbbb1bde09a8ee21fe2401828e2e0dc1fad5
saidi2199 <150718683+saidi2199@users.noreply.github.com>
2023-11-13 22:39:00 +0100

    updated mathquiz

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,22 +1,27 @@
 import random
 
 
-def function_A(min, max):
+def mainFunc(min, max):
     """
     Random integer.
     """
     return random.randint(min, max)
 
-
-def function_B():
+    
+def compute():
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
+def mainLogic(n1, n2, o):
+    if o not in {'+', '-', '*'}:
+        raise ValueError("Invalid operator")
+    
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+':
+        a = n1 + n2
+    elif o == '-':
+        a = n1 - n2
+    else:
+        a = n1 * n2
     return p, a
 
 def math_quiz():
@@ -26,21 +31,23 @@ def math_quiz():
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(int(t_q)):
+        n1 = mainFunc(1, 10)
+        n2 = mainFunc(1, 5)
+        o = compute()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = mainLogic(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {s}/{int(t_q)}")
 
 if __name__ == "__main__":
     math_quiz()
